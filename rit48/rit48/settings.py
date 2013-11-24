009675ecf66641b07cd481231447f261985a1125
Ryan Serazin <rps9530@rit.edu>
2013-11-23 20:42:50 -0500

    deployment is hard

--- rit48/rit48/settings.py
+++ rit48/rit48/settings.py
@@ -74,6 +74,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     STATIC_PATH,
+    '/home/ryan/code/rit48/48hrs/rit48/static',
 )
 
 # List of finder classes that know how to find static files in
